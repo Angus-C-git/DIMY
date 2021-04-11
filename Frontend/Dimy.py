4a148c01d39ab7554ce75c31f7669a3b9371ff9c
Zephyr <anguscornall@gmail.com>
2021-04-11 13:06:45 +1000

    UPDATED: test driver with selctions

--- Frontend/Dimy.py
+++ Frontend/Dimy.py
@@ -1,4 +1,5 @@
 import BloomFilter
+import EphID
 
 '''
 TEST CODE ONLY
@@ -6,23 +7,45 @@ TEST CODE ONLY
 
 
 def run_tests():
-    print("[>>] Running tests in debug mode, pray ...\n")
+    print("=" * 10, " Select a test suite ", "=" * 10, "\n")
+    print("[1] EphID Test")
+    print("[2] Broadcast / Receive shares test")
+    print("[3] EncID Exchange test")
+    print("[4] Bloom Filter tests")
+    print("[5] Full Operations Test (Requires secondary client)")
+    print("[6] Quit")
+    test_selection = int(input("[>>] "))
 
-    print("=" * 10, "DBF Tests", "=" * 10)
-    print("[**] Creating DBF")
-    dbf_1 = BloomFilter.DailyBloomFilter("DBF1")
-    print(f"[**] Created {dbf_1.name}")
-    print(f"[**] Updating {dbf_1.name}'s age")
-    dbf_1.update_age()
-    print(f"[**] {dbf_1.name} AGE: {dbf_1.age}")
+    None if test_selection != 6 else exit(0)
+
+    print("\n[>>] Running tests, pray ...\n")
+
+    if test_selection == 1 or test_selection == 5:
+        print("=" * 10, "EphID Tests", "=" * 10)
+        print("[**] Generating new EphID")
+        eph_id = EphID.EphID()
+        print(f"[**] Created EphID: {eph_id.eph_id}")
+        print("=" * (20 + len(" EphID Tests ")), "\n")
+        run_tests() if test_selection != 5 else None  # test done
+
+    if test_selection == 4 or test_selection == 5:
+        print("=" * 11, "DBF Tests", "=" * 11)
+        print("[**] Creating DBF")
+        dbf_1 = BloomFilter.DailyBloomFilter("DBF1")
+        print(f"[**] Created {dbf_1.name}")
+        print(f"[**] Updating {dbf_1.name}'s age")
+        dbf_1.update_age()
+        print(f"[**] {dbf_1.name} AGE: {dbf_1.age}")
+        print("=" * (22 + len(" DBF Tests ")), "\n")
+        run_tests() if test_selection != 5 else None  # test done
 
 
 def main():
     print("[>>] Running DIMY\n")
 
-    ############# TESTS #############
+    # ========== TESTS ============ #
     run_tests()
-    ################################
+    #################################
 
 
 if __name__ == '__main__':
